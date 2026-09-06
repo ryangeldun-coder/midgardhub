9385b3d68a974588c4c59686f39af9917719aa1b
Ryan Geldun <ryangeldun@gmail.com>
2026-09-06 22:14:10 +0200

    deploy: 2026-09-06 22:14

---
+++ _next/static/chunks/app/kafra/terms/page-1101f1751ba3ccea.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2918,8513],{64235:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,98500,23))}},e=>{e.O(0,[8500,8441,3794,7358],()=>e(e.s=64235)),_N_E=e.O()}]);
